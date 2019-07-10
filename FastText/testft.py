c822b26ea999addb2c6749cdb8e62c68839f94de
angela <25697103+angelahahaa@users.noreply.github.com>
2019-07-10 16:40:44 +0000

    added only-one-class version of p@k in ft test modified:   FastText/testft.py

--- FastText/testft.py
+++ FastText/testft.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description='run FastText')
 parser.add_argument('-m','--model',help='model directory e.g. models/epoch5_lr0.1.bin')
 parser.add_argument('-d','--data',help='data directory e.g. .../data/amazon.test.txt')
 parser.add_argument('-k',default=1,type=int)
+parser.add_argument('--one_class',action='store_true', default=False)
 args = parser.parse_args()
 
 MODEL_DIR = args.model
@@ -12,6 +13,11 @@ TEST_DIR = args.data
 k = args.k
 # start
 model = fasttext.load_model(MODEL_DIR)
+def get_pAtk(correct,samples,k,one_class = False):
+    if one_class:
+        return correct/((i+1))*100
+    else:
+        return correct/((i+1)*args.k)*100
 
 with open(TEST_DIR,'r',encoding = "ISO-8859-1") as f:
     lines = f.read().splitlines()
@@ -34,7 +40,7 @@ for i in range(samples):
             correct+=1
     if i%disp==0:
         c = (i+1)/(samples)*100
-        p = correct/((i+1)*args.k)*100
+        p = get_pAtk(correct,samples,args.k,args.one_class)
         print('TEST {:.2f}%, P@1 = {:.2f}'.format(c,p))
-p = correct/(samples*args.k)*100
+p = get_pAtk(correct,samples,args.k,args.one_class)
 print('COMPLETED, P@k = {:.2f}'.format(p))
